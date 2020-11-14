022f571daa2a0d132b6b7e5f5982e8bb17abb713
lanaschimitez <lana.schimitez@gmail.com>
2020-11-14 19:32:43 -0300

    fim

--- Assets/Scripts/CharacterSelect.cs
+++ Assets/Scripts/CharacterSelect.cs
@@ -12,30 +12,28 @@ public class CharacterSelect : MonoBehaviour, ISelectHandler, IDeselectHandler//
     public Button button;
     public GameObject image;
     public MoveUI MoveUI;
+    public GameObject glow;
 
     void Start()
     {
         button = GetComponent<Button>();
         MoveUI = image.GetComponent<MoveUI>();
+        glow = this.gameObject.transform.GetChild(3).gameObject;
+        glow.SetActive(false);
         MoveUI.Disable();
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     //Do this when the selectable UI object is selected.
     public void OnSelect(BaseEventData eventData)
     {
         Debug.Log(this.gameObject.name + " was selected");
+        glow.SetActive(true);
         this.gameObject.transform.localScale = new Vector3(1.2f, 1.2f, 1.2f);
         MoveUI.Enable();
     }
     public void OnDeselect(BaseEventData Data)
     {
         Debug.Log("Deselected " + this.gameObject.name);
+        glow.SetActive(false);
         this.gameObject.transform.localScale = new Vector3(1f, 1f, 1.2f);
         MoveUI.Disable();
     }
