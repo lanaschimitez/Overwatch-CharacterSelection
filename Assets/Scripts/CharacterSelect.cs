5ed693979fbdece298d01ea4b72809d486ce4087
lanaschimitez <lana.schimitez@gmail.com>
2020-11-15 18:40:01 -0300

    Finalização

--- Assets/Scripts/CharacterSelect.cs
+++ Assets/Scripts/CharacterSelect.cs
@@ -13,26 +13,28 @@ public class CharacterSelect : MonoBehaviour, ISelectHandler, IDeselectHandler//
     public GameObject image;
     public MoveUI MoveUI;
     public GameObject glow;
+    public GameObject borda;
+    public Sprite imagem;
 
     void Start()
     {
         button = GetComponent<Button>();
         MoveUI = image.GetComponent<MoveUI>();
         glow = this.gameObject.transform.GetChild(3).gameObject;
+        borda = GameObject.Find("Borda");
         glow.SetActive(false);
         MoveUI.Disable();
     }
     //Do this when the selectable UI object is selected.
     public void OnSelect(BaseEventData eventData)
     {
-        Debug.Log(this.gameObject.name + " was selected");
         glow.SetActive(true);
         this.gameObject.transform.localScale = new Vector3(1.2f, 1.2f, 1.2f);
+        borda.gameObject.GetComponent<Image>().sprite = imagem;
         MoveUI.Enable();
     }
     public void OnDeselect(BaseEventData Data)
     {
-        Debug.Log("Deselected " + this.gameObject.name);
         glow.SetActive(false);
         this.gameObject.transform.localScale = new Vector3(1f, 1f, 1.2f);
         MoveUI.Disable();
---
+++ Assets/Scripts/EffectUI.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectUI : MonoBehaviour
+{
+    public List<GameObject> objectsUI = new List<GameObject>();
+    public float m_SmoothTime = 8.0f;
+    public int count;
+    public bool ativo;
+
+
+    void Start()
+    {
+        ativo = false;
+        foreach (Transform child in transform)
+        {
+            objectsUI.Add(child.gameObject);
+        }
+        InvokeRepeating("Fade", .1f, .2f);
+    }
+
+    void Fade()
+    {
+            objectsUI[count].SetActive(ativo);
+            count ++;
+            if (count > 6)
+            {
+                count = 0;
+            ativo = !ativo;
+            }
+    }
+
+}
+
+
